0c0c2be0d0f862b943fac0692238f510e00f1e25
John <J.G.Lokerse@gmail.com>
2021-02-05 22:21:36 +0100

    Fixed if-check and removed echo's

--- scan-website.sh
+++ scan-website.sh
@@ -2,15 +2,14 @@ website_url=$1
 scan_text=$2
 status_code=$(curl --silent --head $website_url | head -1 | awk '{print $2}')
 
-echo $status_code
-
 if [ "$status_code" != "200" ]
 then
     echo "Website is unreachable"
     exit 1
 fi
 
-if curl --silent $website_url | grep -i "$2"
+if [ -z $(curl --silent $website_url | grep -i "$2") ]
 then
-    echo "hello"
+    echo "Input text was not found on website."
+    exit 1
 fi
